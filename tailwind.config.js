bf7c9ad5475863113336048bc0bfd9d1d2ddc8c8
FaridAliyev <feliyev636@gmail.com>
2024-11-12 13:38:41 +0400

    add contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,25 @@ module.exports = {
 
       backgroundImage: {
         'header-img': "url('/src/images/header.jpg')",
-        'person': "url('/src/images/person2.png')"
+        'person': "url('/src/images/person2.png')",
+        'img1': "url('/src/images/img1.png')",
+        'img2': "url('/src/images/img2.png')",
+        'img3': "url('/src/images/img3.png')",
+        'img4': "url('/src/images/img4.png')",
       },
       colors: {
         'header-bg-color': 'rgba(120, 53, 15, 0.30)',
-        'button-color': 'rgba(120, 53, 15)'
+        'button-color': 'rgba(120, 53, 15)',
+        'footer-color': 'rgba(12,24,30)',
       },
       fontFamily: {
-        PlayfairDisplay: ['Playfair Display', 'serif']
+        PlayfairDisplay: ['Playfair Display', 'serif'],
+        Overpass: ['Overpass', 'sans-serif']
       },
-      height:{
-        custom:'750px'
+      height: {
+        custom: '750px',
+        custom1: '500px',
+        header: '680px'
       }
     },
   },
